3ec0dbb690d9d08b0c4e09b9688a67ceeadb7bba
Swathi C <swathigowda100@gmail.com>
2019-03-17 19:44:33 -0500

    Fix build_dataset_directory.py for python3
    Signed-off-by: Swathi C <swathigowda100@gmail.com>

--- build_dataset_directory.py
+++ build_dataset_directory.py
@@ -54,23 +54,23 @@ test_slices = np.array_split(test_inds - 1, 10)
 #                     no_superpixels=n_superpixels, x_window_size=113, y_window_size=113, images=images,
 #                     depth_bins=depth_bins,depth_min=min_depth, depth_max=max_depth)
 
-print "nBins = ",depth_bins
-print "Avg Type = ",average_type
+print("nBins = ",depth_bins)
+print("Avg Type = ",average_type)
 
-print 'Building training set...'
+print('Building training set...')
 for s in train_slices:
-	images = tuple(s)
-   	create_segments_directory(input_filename=input_file, image_output_filepath=img_filepath_train,
+    images = tuple(s)
+    create_segments_directory(input_filename=input_file, image_output_filepath=img_filepath_train,
                    no_superpixels=n_superpixels, x_window_size=window_size, y_window_size=window_size, images=images,
                    depth_bins=depth_bins,depth_min=min_depth, depth_max=max_depth, depth_type=average_type,
-				   output_images=output_images, index_name='index_' + str(depth_bins) + '_' + str(average_type) + '.txt')
+                   output_images=output_images, index_name='index_' + str(depth_bins) + '_' + str(average_type) + '.txt')
 
-print 'Building validation set...'
+print('Building validation set...')
 for s in test_slices:
-	images = tuple(s)
-	create_segments_directory(input_filename=input_file, image_output_filepath=img_filepath_test,
+    images = tuple(s)
+    create_segments_directory(input_filename=input_file, image_output_filepath=img_filepath_test,
                    no_superpixels=n_superpixels, x_window_size=window_size, y_window_size=window_size, images=images,
                    depth_bins=depth_bins,depth_min=min_depth, depth_max=max_depth, depth_type=average_type,
-				   output_images=output_images, index_name='index_' + str(depth_bins) + '_' + str(average_type) + '.txt')
+                   output_images=output_images, index_name='index_' + str(depth_bins) + '_' + str(average_type) + '.txt')
 
     
